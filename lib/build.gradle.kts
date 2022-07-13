81ccf0a5862fdb4ac6ef29bf949f7d12b1b5372d
Kacper Kafara <kacperkafara@gmail.com>
2022-07-13 13:36:41 +0200

    chore: add gradle code to allow publishing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,6 +3,46 @@ plugins {
     `maven-publish`
 }
 
+group = "com.kkafara.monad"
+version = "1.0.0"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+fun getPropertyOrEnv(propName: String, envVarName: String?): String? {
+    return (project.findProperty(propName) as String? ?: System.getenv(envVarName))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kkafar/java-monads")
+            credentials {
+                username = getPropertyOrEnv("gh.user", "GITHUB_USERNAME")
+                password = getPropertyOrEnv("gh.token", "GITHUB_TOKEN")
+            }
+        }
+        maven {
+            name = "JFrogBintray"
+            url = uri("https://kkafara.jfrog.io/artifactory/kkafara-gradle-release-local")
+            credentials {
+                username = getPropertyOrEnv("jfrog.user", "JFROG_USERNAME")
+                password = getPropertyOrEnv("jfrog.token", "JFROG_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            groupId = "com.kkafara.monad"
+            artifactId = "java-monads"
+            from(components["java"])
+        }
+    }
+}
+
 repositories {
     mavenCentral()
 }
