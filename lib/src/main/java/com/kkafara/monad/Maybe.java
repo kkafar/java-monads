ac8688cb8360321c0935fe5a5280eabb8412f443
Kacper Kafara <kacperkafara@gmail.com>
2022-07-13 12:33:00 +0200

    chore: change API to include only one factory method

--- lib/src/main/java/com/kkafara/monad/Maybe.java
+++ lib/src/main/java/com/kkafara/monad/Maybe.java
@@ -1,6 +1,7 @@
 package com.kkafara.monad;
 
 import com.kkafara.rt.Result;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
@@ -13,35 +14,27 @@ public class Maybe<T> {
     this.value = value;
   }
 
-  public static <S> Maybe<S> of(@Nullable S value) {
-    return new Maybe<>(value);
-  }
-
-  public static <S> Maybe<S> some(@Nullable S value) {
-    assert value != null : "Value passed to some must not be null.";
-    return Maybe.of(value);
-  }
-
-  public static <S> Maybe<S> none() {
-    return Maybe.of(null);
-  }
-
+  @NotNull
   public static <S> Maybe<S> wrap(@Nullable S value) {
     return new Maybe<>(value);
   }
 
+  @Nullable
   public T unwrap() {
     return value;
   }
 
+  @NotNull
   public Result<T, Void> unwrapToResult() {
     return value == null ? Result.err() : Result.ok(value);
   }
 
+  @NotNull
   public <R> Maybe<R> transform(Function<T, R> f) {
     return new Maybe<>(f.apply(value));
   }
 
+  @NotNull
   public Maybe<T> transformInPlace(Function<T, T> f) {
     value = f.apply(value);
     return this;
