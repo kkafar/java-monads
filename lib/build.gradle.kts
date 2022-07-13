e98d4388eef40d88eeb574504f3b647bcf3ebf10
Kacper Kafara <kacperkafara@gmail.com>
2022-07-13 12:26:25 +0200

    chore: add dependency on com.kkafara.rt:result-type

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,19 +1,20 @@
 plugins {
-    // Apply the java-library plugin for API and implementation separation.
     `java-library`
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
+    maven {
+        name = "JFrogBintray"
+        url = uri("https://kkafara.jfrog.io/artifactory/kkafara-gradle-release-local/")
+    }
 }
 
 dependencies {
-    // Use JUnit test framework.
     testImplementation("junit:junit:4.13.2")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:30.1.1-jre")
+    api("com.kkafara.rt:result-type:1.0.3")
 
+    implementation("com.google.guava:guava:30.1.1-jre")
     implementation("org.jetbrains:annotations:23.0.0")
 }
